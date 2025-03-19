c789e61bb0f7b5162f1e1f240f14f42de01a4ed8
Lee-Dongwook <dlehddnr0713@gmail.com>
2025-03-19 15:54:17 +0900

    feat : init

---
+++ example/src/components/example/index.tsx
@@ -0,0 +1,12 @@
+export const ExamplePage = () => {
+  return (
+    <div className="p-10">
+      <div className="flex flex-row items-center justify-between">
+        <h2 className="text-2xl font-bold">포트폴리오</h2>
+        <button className="bg-blue-500 text-white h-10 rounded-lg">
+          포트폴리오 추가
+        </button>
+      </div>
+    </div>
+  );
+};
